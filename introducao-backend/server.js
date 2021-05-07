53125a3990c98e04869b7c6859fc4e44bac3dde1
Patrick Freitas <patrickfreitas.dev@gmail.com>
2021-05-07 13:04:14 -0300

    Retornando estudos

---
+++ introducao-backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+server.use(express.static('public'))
+
+server.set('view engine', 'html')
+
+nunjucks.configure('views', {
+  express: server,
+  noCache: true
+})
+
+server.get('/', function (req, res) {
+  return res.render('about')
+})
+
+server.get('/portfolio', function (req, res) {
+  return res.render('portfolio')
+})
+
+
+server.listen(5000, function () {
+  console.log('Server is running')
+})
